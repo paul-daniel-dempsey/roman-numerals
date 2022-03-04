c264f8648b2211ec84afbb4057b0fe13365d5f05
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2022-03-04 10:07:02 +0000

    decToRoman TestA convert single numeral

--- src/roman-numerals.js
+++ src/roman-numerals.js
@@ -1,9 +1,11 @@
 
 const decToRoman = (dec) => {
-    return 'I';
-
-
-
+    return decToRomanTestA(dec)
+}
+const decToRomanTestA = (dec) => {
+    
+    let convert = {0:'',1:'I',5:'V',10:'X',50:'L',100:'C',500:'D',1000:'M'};
+    return convert[dec];
 }
 
 const romanToDec = (numeral) => {
--- src/roman-numerals.test.js
+++ src/roman-numerals.test.js
@@ -11,12 +11,12 @@ const { decToRoman, romanToDec } = require("./roman-numerals");
 // (D) Sample includes EVERY Roman Numeral Added Together (1000+500+100+50+10+1=1666)
 
 describe("decToRoman", () => {
-    // it.each([[1,'I'],[5,'V'],[10,'X'],[50,'L'],[100,'C'],[500,'D'],[1000,'M']])
-    // (`(A) Check %i for Roman Equivalent`,
-    //     (dec,result) => {
-    //         expect(decToRoman(dec)).toEqual(result);
-    //     }    
-    // )
+    it.each([[1,'I'],[5,'V'],[10,'X'],[50,'L'],[100,'C'],[500,'D'],[1000,'M']])
+    (`(A) Check %i for Roman Equivalent`,
+        (dec,result) => {
+            expect(decToRoman(dec)).toEqual(result);
+        }    
+    )
     // test('(B) Size Test Not > 3000 ', () => {
     //     expect(decToRoman(3001)).toEqual("");
     // })
@@ -36,20 +36,20 @@ describe("decToRoman", () => {
 // Inverse Of Pt1 (A-D)
 
 describe("romanToDec", () => {
-    it.each([['I',1],['V',5],['X',10],['L',50],['C',100],['D',500],['M',1000]])
-    (`(A) Check %s for Decimal Equivalent`,
-        (numeral,result) => {
-            expect(romanToDec(numeral)).toEqual(result);
-        }    
-    )
-    test('(B) Size Test Not > 3000 ', () => {
-        expect(romanToDec("MMMI")).toEqual(0);
-    })
-    test('(C) Size Test <= 0 ', () => {
-        expect(romanToDec("")).toEqual(0);
-    })
-    test('(D) Check Decimals (All + Double Numeral) ', () => {
-        expect(romanToDec("MDCLXVI")).toEqual(1666);
-        expect(romanToDec("MMI")).toEqual(2001);
-    }) 
+    // it.each([['I',1],['V',5],['X',10],['L',50],['C',100],['D',500],['M',1000]])
+    // (`(A) Check %s for Decimal Equivalent`,
+    //     (numeral,result) => {
+    //         expect(romanToDec(numeral)).toEqual(result);
+    //     }    
+    // )
+    // test('(B) Size Test Not > 3000 ', () => {
+    //     expect(romanToDec("MMMI")).toEqual(0);
+    // })
+    // test('(C) Size Test <= 0 ', () => {
+    //     expect(romanToDec("")).toEqual(0);
+    // })
+    // test('(D) Check Decimals (All + Double Numeral) ', () => {
+    //     expect(romanToDec("MDCLXVI")).toEqual(1666);
+    //     expect(romanToDec("MMI")).toEqual(2001);
+    // }) 
 })
